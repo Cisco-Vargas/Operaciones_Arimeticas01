b55c762cc8020a5a85df7f5067def8d31db66066
Cisco-Vargas <103206821+Cisco-Vargas@users.noreply.github.com>
2022-04-07 13:18:27 -0300

    Add files via upload

---
+++ Multiplicacion en python.py
@@ -0,0 +1,8 @@
+#Operacion de multiplicacion
+
+sumatoria01 = int (input ("Digitar el 1er numero: ") )
+sumatoria02 = int (input ("Digitar el 2do numero: ") )
+
+Resultado = sumatoria01*sumatoria02
+
+print (f"La multiplicacion de los 2 numero es: {Resultado}" )
---
+++ Operacion de DIVISION en python.py
@@ -0,0 +1,8 @@
+#Operacion de DIVISION
+
+sumatoria01 = int (input ("Digitar el 1er numero: ") )
+sumatoria02 = int (input ("Digitar el 2do numero: ") )
+
+Resultado = sumatoria01/sumatoria02
+
+print (f"La division de los 2 numero es: {Resultado}" )
